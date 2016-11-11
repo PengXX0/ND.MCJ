5189bc90ba6f34817de95b96d90fd9263d0c4fd1
pengxingxing <peng.xingxing@itr.cn>
2016-11-11 15:12:55 +0800

    update

--- Hosting/ND.MCJ.WebApi/Areas/Help/Document/dist/js/customer.js
+++ Hosting/ND.MCJ.WebApi/Areas/Help/Document/dist/js/customer.js
@@ -45,6 +45,50 @@
             } else { serializeObj[this.name] = this.value; }
         }); return serializeObj;
     },
+    validator: function () {
+        $('#tryitForm').bootstrapValidator({
+            fields: {
+                firstName: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The first name is required and cannot be empty'
+                        }
+                    }
+                },
+                lastName: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The last name is required and cannot be empty'
+                        }
+                    }
+                },
+                email: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The email address is required'
+                        },
+                        emailAddress: {
+                            message: 'The input is not a valid email address'
+                        }
+                    }
+                },
+                gender: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The gender is required'
+                        }
+                    }
+                }
+            },
+            submitHandler: function (validator, form, submitButton) {
+                var fullName = [validator.getFieldElements('firstName').val(),
+                                validator.getFieldElements('lastName').val()].join(' ');
+                $('#helloModal')
+                    .find('.modal-title').html('Hello ' + fullName).end()
+                    .modal();
+            }
+        });
+    },
     init: function () {
         window.onresize = this.resizeStyle; $(window).resize();
         $(".menu").find("li ul").prev("a").css("padding-left", "0").prepend('<i class="arrow-right"></i>');
---
+++ Hosting/ND.MCJ.WebApi/Filters/ValidataModelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace NC.MCJ.WebApi.Filters
+{
+    public class ValidataModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ModelState.IsValid) return;
+            var errors = new Dictionary<String, IEnumerable<String>>();
+            actionContext.ModelState.ToList().ForEach(s => { errors[s.Key] = s.Value.Errors.Select(e => e.ErrorMessage); });
+            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.OK,
+            new { code = HttpStatusCode.BadRequest, message = errors.FirstOrDefault().Value.FirstOrDefault() });
+        }
+    }
+}
